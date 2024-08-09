cf5c92e5fbe2fdb7f589c0b1cd183b0abf81af34
Artorias-8546 <luisporfirio2006@gmail.com>
2024-08-09 14:48:39 -0300

    segundo commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,18 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  pessoa = {
+    foto: 'https://professionalmoron.com/wp-content/uploads/2012/05/alpaca-985158_640.jpg',
+    nome: 'Barrigudinha Seleide',
+    objetivo: 'Programador HTML & CSS',
+    contato: {
+      telefone: '15999999999',
+      email: 'seleide@hotmail',
+      github: 'github.com/seleide',
+      linkedin: 'linkedin.'
+    }
+  }
 
   constructor(){ }
 }

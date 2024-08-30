1f01a9acd7060815a3460d239be3bf2b744d6795
Artorias-8546 <luisporfirio2006@gmail.com>
2024-08-30 14:47:43 -0300

    cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,23 @@ const routes: Routes = [
   {
     path: 'ver-detalhes-pais',
     loadChildren: () => import('./ver-detalhes-pais/ver-detalhes-pais.module').then( m => m.VerDetalhesPaisPageModule)
+  },  {
+    path: 'cadastrar-funcionario',
+    loadChildren: () => import('./cadastrar-funcionario/cadastrar-funcionario.module').then( m => m.CadastrarFuncionarioPageModule)
+  },
+  {
+    path: 'listar-funcionario',
+    loadChildren: () => import('./listar-funcionario/listar-funcionario.module').then( m => m.ListarFuncionarioPageModule)
+  },
+  {
+    path: 'excluir-funcionario',
+    loadChildren: () => import('./excluir-funcionario/excluir-funcionario.module').then( m => m.ExcluirFuncionarioPageModule)
+  },
+  {
+    path: 'atualizar-funcionario',
+    loadChildren: () => import('./atualizar-funcionario/atualizar-funcionario.module').then( m => m.AtualizarFuncionarioPageModule)
   }
+
 ];
 
 @NgModule({
